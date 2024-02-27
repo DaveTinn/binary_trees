d1d61162f655c1a38c1b76d89c112a1634debff7
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-27 14:08:42 -0800

    A function checking if a node is a leaf

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -30,5 +30,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = newNode;
 	}
 	parent->left = newNode;
-	return (newNode->left);
+	return (newNode);
 }
