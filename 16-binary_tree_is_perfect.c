0519c46781ae4fd81133b13082012e782b91d398
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-02-29 10:22:04 -0800

    A function checking if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,7 +47,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		l_perfect = binary_tree_is_perfect(tree->left);
 		r_perfect = binary_tree_is_perfect(tree->right);
 
-		return (l_perfect && r_perfect);
+		if (l_perfect && r_perfect)
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
